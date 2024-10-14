ddcef59cd8d3fc70780025e15f05b08be8e4dc94
Nikolah McWin <nikolah.mcwin@gmail.com>
2024-10-14 11:58:42 +1000

    Updating API to read data from JSON file

--- backend/server.js
+++ backend/server.js
@@ -3,69 +3,28 @@
    @author Nikolah McWin
 */
 
-
 // Initialise dependencies
 const express = require("express");
 const swaggerUI = require("swagger-ui-express");
 const YAML = require("yamljs");
 const swaggerDoc = YAML.load("./swagger.yaml");
 
+// 
 const app = express();
 const port = 3000;
 
-//Mock backend cheese data
-let basicCheeses = [{ "id": 1, "name": "Asiago" }, { "id": 2, "name": "Brie" }, { "id": 3, "name": "Camembert" }, { "id": 4, "name": "Danish Blue" }, { "id": 5, "name": "Emmental" }]
-let cheeses = [{
-   "id": 1,
-   "name": "Asiago",
-   "origin": "Italy",
-   "price_per_kilo": 19.50,
-   "colour": "Pale Yellow",
-   "texture": "Firm",
-   "milk_type": "Cow",
-   "photo-url": "https://www.cheese.com/media/img/cheese/Formaggio_Asiago_Dop_mdAjQor.jpg"
-},
-{
-   "id": 2,
-   "name": "Brie",
-   "origin": "France",
-   "price_per_kilo": 22.30,
-   "colour": "White",
-   "texture": "Soft",
-   "milk_type": "Cow",
-   "photo-url": "https://www.cheese.com/media/img/cheese-suggestion/Briefermier.jpg"
-},
-{
-   "id": 3,
-   "name": "Camembert",
-   "origin": "France",
-   "price_per_kilo": 25.00,
-   "colour": "Cream",
-   "texture": "Soft",
-   "milk_type": "Cow",
-   "photo-url": "https://www.cheese.com/media/img/cheese-suggestion/CamembertStLoup_800x.jpg"
-},
-{
-   "id": 4,
-   "name": "Danish Blue",
-   "origin": "Denmark",
-   "price_per_kilo": 21.80,
-   "colour": "Blue",
-   "texture": "Semi-Soft",
-   "milk_type": "Cow",
-   "photo-url": "https://www.cheese.com/media/img/cheese/Danablu-Danish-Blue.jpg"
-},
-{
-   "id": 5,
-   "name": "Emmental",
-   "origin": "Switzerland",
-   "price_per_kilo": 24.50,
-   "colour": "Yellow",
-   "texture": "Firm",
-   "milk_type": "Cow",
-   "photo-url": "https://www.cheese.com/media/img/cheese-suggestion/Emmental_6174830f-d536-4b99-964e-ea52f1ed6a57_800x.jpg"
-}];
+// Pull in cheese data from JSON file
+const fs = require("fs");
+let cheeses = '';
+fs.readFile("cheeses.json", "utf8", (err, jsonString) => {
+  if (err) {
+    console.log("File read failed:", err);
+    return;
+  }
+  cheeses = JSON.parse(jsonString).cheeses;
+});
 
+console.log(cheeses);
 //Middleware for JSON and SWAGGER
 app.use(express.json());
 app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerDoc));
@@ -77,12 +36,10 @@ app.get("/", (req, res) => {
 
 // All cheeses page
 app.get("/cheeses", (req, res) => {
-   res.status(200);
    res.send(cheeses);
 })
 
-
-//Single cheese by ID
+// Single cheese by ID
 app.get("/cheeses/:id", (req, res) => {
    const reqId = parseInt(req.params.id);
    const cheese = cheeses.find(i => i.id === reqId);
