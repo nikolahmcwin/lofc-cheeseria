fb72efaca6f0ffe0159f692cd1871044c665b377
Nikolah McWin <nikolah.mcwin@gmail.com>
2024-10-15 10:16:58 +1000

    Final API methods tidied and tested, swagger documentation updated, and uninstalled unused swagger-ui package.

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,6 @@
    @author Nikolah McWin
 */
 
-
 // Initialise dependencies
 const swaggerUI = require("swagger-ui-express");
 const YAML = require("yamljs");
@@ -14,7 +13,8 @@ const app = express();
 
 const port = 3000;
 const fileName = "cheeses.json";
-const newFileName = "new_cheeses.json";
+const newFileName = fileName;
+//const newFileName = "new_cheeses.json";
 //TODO remove double file name
 
 // Add middleware 
@@ -22,13 +22,13 @@ app.use(express.json());
 app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerDoc));
 
 // Used to pull the cheese data out of the JSON file
-let cheeses = '';
+let cheeses = "";
 fs.readFile("cheeses.json", "utf8", (err, jsonString) => {
-  if (err) {
-    console.log("File read failed:", err);
-    return;
-  }
-  cheeses = JSON.parse(jsonString);
+   if (err) {
+      console.log("File read failed:", err);
+      return;
+   }
+   cheeses = JSON.parse(jsonString);
 });
 
 // Used to write data out to the JSON file
@@ -36,11 +36,11 @@ function writeData() {
    const jsonString = JSON.stringify(cheeses, null, 2);
    fs.writeFile(newFileName, jsonString, err => {
       if (err) {
-          console.log("Error writing file", err)
+         console.log("Error writing file", err)
       } else {
-          console.log("Successfully wrote file")
+         //console.log("Successfully wrote file")
       }
-  })
+   })
 }
 
 // Home page 
@@ -74,34 +74,43 @@ app.post("/cheeses", (req, res) => {
 
 // Get cheese by ID
 app.get("/cheeses/:id", (req, res) => {
-   const reqId = parseInt(req.params.id);
-   const cheese = cheeses.find(i => i.id === reqId);
 
-   if (cheese != null) {
-      res.send(cheese);
-   } else {
-      res.status(404).send("Cheese not found");
+   const reqId = parseInt(req.params.id);
+   if (Number.isNaN(reqId)) {
+      return res.status(400).send("Invalid cheese ID");
    }
+
+   const cheese = cheeses.find(i => i.id == reqId);
+   if (cheese == null) {
+      return res.status(404).send(`No cheese found for ID: ${reqId}`);
+   }
+   res.send(cheese);
 });
 
 // Update cheese by ID
 app.put("/cheeses/:id", (req, res) => {
-   const reqId = parseInt(req.params.id);
-   const index = cheeses.findIndex(i => i.id === reqId); 
 
-   if (index === -1) {
-         res.status(404).send("Cheese not found");
-   } else {
-      cheeses[index].name = req.body.name;
-      cheeses[index].origin = req.body.origin;
-      cheeses[index].price = req.body.price;
-      cheeses[index].colour = req.body.colour;
-      cheeses[index].texture = req.body.texture;
-      cheeses[index].milk = req.body.milk;
-      cheeses[index].photo = req.body.photo;
-      res.send(cheeses[index]);
-      writeData();
+   const reqId = parseInt(req.params.id);
+   if (Number.isNaN(reqId)) {
+      return res.status(400).send("Invalid cheese ID");
    }
+
+   const index = cheeses.findIndex(i => i.id == reqId);
+   if (index == -1) {
+      return res.status(404).send(`No cheese found for ID: ${reqId}`);
+   }
+
+   cheeses[index].name = req.body.name;
+   cheeses[index].origin = req.body.origin;
+   cheeses[index].price = req.body.price;
+   cheeses[index].colour = req.body.colour;
+   cheeses[index].texture = req.body.texture;
+   cheeses[index].milk = req.body.milk;
+   cheeses[index].photo = req.body.photo;
+
+   res.send(cheeses[index]);
+   writeData();
+
 
 });
 
@@ -109,14 +118,19 @@ app.put("/cheeses/:id", (req, res) => {
 app.delete("/cheeses/:id", (req, res) => {
 
    const reqId = parseInt(req.params.id);
-   const cheese = cheeses.find(i => i.id === reqId);
-
-   if (cheese != null) {
-      //TODO DELETE cheese by ID
-
-   } else {
-      res.status(404).send("Cheese not found");
+   if (Number.isNaN(reqId)) {
+      return res.status(400).send("Invalid cheese ID");
    }
+
+   const index = cheeses.findIndex(i => i.id == reqId);
+   if (index == -1) {
+      return res.status(404).send(`No cheese found for ID: ${reqId}`);
+   }
+
+   const cheese = cheeses.splice(index, 1);
+   res.send(cheese[0]);
+   writeData();
+
 });
 
 
