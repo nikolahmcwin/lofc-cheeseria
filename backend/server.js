e8183da0187dc698d6915716e0273921fbc3cabf
Nikolah McWin <nikolah.mcwin@gmail.com>
2024-10-16 21:54:27 +1000

    Rewriting problem backend to properly handle reading and writing file data safely, removing global CHEESES array, replacing it with correct file reads as appropriate. Also updated to require FS promises instead of just FS.

--- backend/server.js
+++ backend/server.js
@@ -1,62 +1,71 @@
-/*
-   Back end server for Cheeseria POC
-   @author Nikolah McWin
-*/
+/**
+ * Basic back end server for LoFC Cheeseria POC.
+ *
+ * @author Nikolah McWin
+ * October 2024
+ */
 
 // Initialise dependencies
 const swaggerUI = require("swagger-ui-express");
 const YAML = require("yamljs");
 const swaggerDoc = YAML.load("./swagger.yaml");
-const fs = require("fs");
+const fs = require("fs").promises;
 const express = require("express");
+const cors = require("cors");
 const app = express();
-const cors = require('cors');
-
 
 // Constants
 const PORT = 3000;
 const FILE_NAME = "cheeses.json";
 
-// Add middleware 
+// Add middleware
 app.use(express.json());
 app.use(cors());
 app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerDoc));
 
 // Used to pull the cheese data out of the JSON file
-// TODO remove global variable + read data directly when required
-let cheeses = "";
-fs.readFile(FILE_NAME, "utf8", (err, jsonString) => {
-   if (err) {
-      console.error(`Reading file ${FILE_NAME} failed with error: `, err);
-      return;
-   }
-   cheeses = JSON.parse(jsonString);
-});
-
-// Used to write data out to the JSON file
-function writeData() {
-   const jsonString = JSON.stringify(cheeses, null, 2);
-   fs.writeFile(FILE_NAME, jsonString, err => {
-      if (err) {
-         console.error(`Writing file ${FILE_NAME} failed with error: `, err);
-      }
-   })
+async function readData() {
+  try {
+    const jsonString = await fs.readFile(FILE_NAME, "utf8");
+    return JSON.parse(jsonString);
+  } catch (err) {
+    console.error(`Reading file ${FILE_NAME} failed with error: `, err);
+    throw new Error("Error reading data.");
+  }
 }
 
-// Home page 
+// Used to write data out to the JSON file
+async function writeData(data) {
+  const jsonString = JSON.stringify(data, null, 2);
+  await fs.writeFile(FILE_NAME, jsonString, (err) => {
+    if (err) {
+      console.error(`Writing file ${FILE_NAME} failed with error: `, err);
+      throw new Error("Error writing data.");
+    }
+  });
+}
+
+//Home page
 app.get("/", (req, res) => {
-   res.send("Home page");
-   //TODO do we need the homepage?
-})
+  res.send("Home Page");
+});
 
 // Get all cheeses
-app.get("/cheeses", (req, res) => {
-   res.send(cheeses);
-})
+app.get("/cheeses", async (req, res) => {
+  try {
+    const cheeses = await readData();
+    res.send(cheeses);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Error fetching data");
+  }
+});
 
 // Add to all cheeses
-app.post("/cheeses", (req, res) => {
-   const newCheese = {
+app.post("/cheeses", async (req, res) => {
+  try {
+    const cheeses = await readData();
+    const newCheese = {
       id: cheeses.length + 1,
       name: req.body.name,
       origin: req.body.origin,
@@ -64,43 +73,54 @@ app.post("/cheeses", (req, res) => {
       colour: req.body.colour,
       texture: req.body.texture,
       milk: req.body.milk,
-      photo: req.body.photo
-   };
+      photo: req.body.photo,
+    };
 
-   cheeses.push(newCheese);
-   res.status(201).send(newCheese);
-   writeData();
-})
+    cheeses.push(newCheese);
+    await writeData(cheeses);
+    res.status(201).send(newCheese);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Error posting data");
+  }
+});
 
 // Get cheese by ID
-app.get("/cheeses/:id", (req, res) => {
+app.get("/cheeses/:id", async (req, res) => {
+  const reqId = parseInt(req.params.id);
+  if (Number.isNaN(reqId)) {
+    return res.status(400).send("Invalid cheese ID");
+  }
 
-   const reqId = parseInt(req.params.id);
-   if (Number.isNaN(reqId)) {
-      return res.status(400).send("Invalid cheese ID");
-   }
-
-   const cheese = cheeses.find(i => i.id == reqId);
-   if (cheese == null) {
+  try {
+    const cheeses = await readData();
+    const cheese = cheeses.find((i) => i.id == reqId);
+    if (cheese == null) {
       return res.status(404).send(`No cheese found for ID: ${reqId}`);
-   }
-   res.send(cheese);
+    }
+
+    res.send(cheese);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Error getting data");
+  }
 });
 
 // Update cheese by ID
-app.put("/cheeses/:id", (req, res) => {
+app.put("/cheeses/:id", async (req, res) => {
+  const reqId = parseInt(req.params.id);
+  if (Number.isNaN(reqId)) {
+    return res.status(400).send("Invalid cheese ID");
+  }
 
-   const reqId = parseInt(req.params.id);
-   if (Number.isNaN(reqId)) {
-      return res.status(400).send("Invalid cheese ID");
-   }
-
-   const index = cheeses.findIndex(i => i.id == reqId);
-   if (index == -1) {
+  try {
+    const cheeses = await readData();
+    const index = cheeses.findIndex((i) => i.id == reqId);
+    if (index == -1) {
       return res.status(404).send(`No cheese found for ID: ${reqId}`);
-   }
+    }
 
-   let newCheese = {
+    let newCheese = {
       id: reqId,
       name: req.body.name,
       origin: req.body.origin,
@@ -108,36 +128,41 @@ app.put("/cheeses/:id", (req, res) => {
       colour: req.body.colour,
       texture: req.body.texture,
       milk: req.body.milk,
-      photo: req.body.photo
+      photo: req.body.photo,
     };
 
-   cheeses[index] = newCheese;
-   res.send(cheeses[index]);
-   writeData();
+    cheeses[index] = newCheese;
+    await writeData(cheeses);
+    res.send(cheeses[index]);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Error updating data");
+  }
 });
 
 // Delete cheese by ID
-app.delete("/cheeses/:id", (req, res) => {
-
-   const reqId = parseInt(req.params.id);
-   if (Number.isNaN(reqId)) {
-      return res.status(400).send("Invalid cheese ID");
-   }
-
-   const index = cheeses.findIndex(i => i.id == reqId);
-   if (index == -1) {
+app.delete("/cheeses/:id", async (req, res) => {
+  const reqId = parseInt(req.params.id);
+  if (Number.isNaN(reqId)) {
+    return res.status(400).send("Invalid cheese ID");
+  }
+  try {
+    const cheeses = await readData();
+    const index = cheeses.findIndex((i) => i.id == reqId);
+    if (index == -1) {
       return res.status(404).send(`No cheese found for ID: ${reqId}`);
-   }
+    }
 
-   const cheese = cheeses.splice(index, 1);
-   res.send(cheese[0]);
-   writeData();
-
+    const cheese = cheeses.splice(index, 1);
+    await writeData(cheeses);
+    res.send(cheese[0]);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Error deleting data");
+  }
 });
-
 
 // Set the app to listen
 app.listen(PORT, () => {
-   console.log(`Server running at http://localhost:${PORT}/`)
+  console.log(`Server running at http://localhost:${PORT}/`);
 });
-
