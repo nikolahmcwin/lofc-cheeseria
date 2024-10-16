015f021c1f8058cd2258eef63e41d5ba4db50138
Nikolah McWin <nikolah.mcwin@gmail.com>
2024-10-16 14:25:29 +1000

    Initialising new front-end react project with Vite, and replacing minor base code and SVGs

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+//import cheeseLogo from "./assets/cheese-blob-2.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div>
+        <h1>Hello world!</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
